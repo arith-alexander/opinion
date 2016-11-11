251f337785240355fc8ab30ad1330c08054f9b35
arith-alexander <alexander.k@arith-metic.jp>
2016-11-11 17:25:26 +0900

    #19 質問をDBから読むように対応しました

--- frontend/opinion.php
+++ frontend/opinion.php
@@ -21,13 +21,13 @@ $sql = "SELECT * FROM question";
 $result = $conn->query($sql);
 
 if (!$result) {
-    echo "empty";
+    die("question is empty");
 }
 
-if ($result->num_rows > 0) {
-    print_r($result->fetch_all());
+$questions = array();
+while ($question = $result->fetch_assoc()) {
+    $questions[] = $question;
 }
-
 ?>
 
 <html>
@@ -37,11 +37,10 @@ if ($result->num_rows > 0) {
 <body>
 <form name="input" action="opinion.php" method="post">
 
-名前
-<br>
-<input type="text" name="name" size="40">
-<br>
-年齢
+<?php
+foreach ($questions as $question) { ?>
+
+<?=$question["content"]?>
 <br>
 <input type="radio" name="age" value="0">〜19
 <input type="radio" name="age" value="1" checked="checked">20〜29
@@ -50,32 +49,9 @@ if ($result->num_rows > 0) {
 <input type="radio" name="age" value="4">50〜59
 <input type="radio" name="age" value="5">60〜
 <br>
-性別
-<br>
-<input type="radio" name="sex" value="0" checked="checked">男性
-<input type="radio" name="sex" value="1">女性
-<br><br>
-映画「君の名は。」についてどう思いますか？
-<br>
-<input type="radio" name="question1" value="0" checked="checked">よいと思う
-<input type="radio" name="question1" value="1">どちらかといえばよい
-<input type="radio" name="question1" value="2">どちらかといえば悪い
-<input type="radio" name="question1" value="3">悪いと思う
-<br>
-Nintendo Switchについてどう思いますか？
-<br>
-<input type="radio" name="question2" value="0" checked="checked">よいと思う
-<input type="radio" name="question2" value="1">どちらかといえばよい
-<input type="radio" name="question2" value="2">どちらかといえば悪い
-<input type="radio" name="question2" value="3">悪いと思う
-<br>
-トランプが大統領になったことに対してどう思いますか？
-<br>
-<input type="radio" name="question3" value="0" checked="checked">よいと思う
-<input type="radio" name="question3" value="1">どちらかといえばよい
-<input type="radio" name="question3" value="2">どちらかといえば悪い
-<input type="radio" name="question3" value="3">悪いと思う
-<br>
+
+<?php }?>
+
 <input type="submit" value="回答">
 </form>
 </body>
