e3dfa8941250b4d1362ae378962b38437f0c77a3
arith-Harrell <m.harrell@arith-metic.jp>
2016-11-11 17:36:17 +0900

    #25 basic intro page

---
+++ frontend/header.php
@@ -0,0 +1,28 @@
+<?php
+/*<script type="text/javascript" src="scripts/juic/js/jquery-1.10.2.js"></script>
+	//<script type="text/javascript" src="scripts/juic/js/jquery-ui-1.10.4.custom.min.js"></script>	
+
+ui-tabs: The outer container of the tabs. This element will additionally have a class of ui-tabs-collapsible when the collapsible option is set.
+ui-tabs-nav: The list of tabs.
+The active list item in the nav will have a ui-tabs-active class. Any list item whose associated content is loading via an Ajax call will have a ui-tabs-loading class.
+ui-tabs-anchor: The anchors used to switch panels.
+ui-tabs-panel: The panels associated with the tabs. Only the panel whose corresponding tab is active will be visible.
+*/
+	
+echo'
+
+<!doctype html>
+<html lang="jp">
+<head>
+	<meta charset="UTF-8">
+	
+	<link rel="stylesheet" type="text/css" href="css/Grid.css">
+    <link rel="stylesheet" type="text/css" href="css/OpinionStyle.css">
+	<link href="https://fonts.googleapis.com/css?family=Fjord+One" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Amaranth:700" rel="stylesheet" type="text/css">
+	';
+echo '<title>Opinion Survey</title>';
+
+echo'</head>';
+
+?>
