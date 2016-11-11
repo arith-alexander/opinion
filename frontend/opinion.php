8852166654f9b347be6f3d309aa4e1b8a9db8139
arith-k-saito <k.saito@arith-metic.jp>
2016-11-11 18:36:01 +0900

    #12 回答の保存を修正した

--- frontend/opinion.php
+++ frontend/opinion.php
@@ -48,7 +48,7 @@ while ($choice = $result->fetch_assoc()) {
 <title>アンケート</title>
 </head>
 <body>
-<form name="input" action="opinion.php" method="post">
+<form name="input" action="save.php" method="post">
 
 <?php
 foreach ($questions as $question) { ?>
@@ -68,40 +68,3 @@ foreach ($questions as $question) { ?>
 </form>
 </body>
 </html>
-
-<?php
-/*
- * アンケート回答を保存する
- */
-
-define("QUESTION_NUM",		3);
-define("AGE_CHOICES_NUM",	6);
-define("SEX_CHOICES_NUM",	2);
-define("QUESTION_CHOICES_NUM",	4);
-
-$answerList = array();
-
-// 名前入力
-if ( !isset($_POST["name"]) || empty($_POST["name"])) {
-	exit;
-}
-$name = strval($_POST["name"]);
-// 年齢入力
-if ( !isset($_POST["age"]) || !is_numeric($_POST["age"]) || $_POST["age"] < 0 || $_POST["age"] > AGE_CHOICES_NUM ) {
-	exit;
-}
-$age= intval($_POST["age"]);
-// 性別入力
-if ( !isset($_POST["sex"]) || !is_numeric($_POST["sex"]) || $_POST["sex"] < 0 || $_POST["sex"] > SEX_CHOICES_NUM ) {
-	exit;
-}
-$sex= intval($_POST["sex"]);
-// 質問の回答入力
-for ( $qno = 1; $qno <= QUESTION_NUM; $qno++ ) {
-	if ( !isset($_POST["question".$qno]) || !is_numeric($_POST["question".$qno]) || $_POST["question".$qno] < 0 || $_POST["question".$qno] > QUESTION_CHOICES_NUM ) {
-		exit;
-	}
-	$answerList[$qno] = intval($_POST["question".$qno]);
-}
-
-// TODO: データ保存
